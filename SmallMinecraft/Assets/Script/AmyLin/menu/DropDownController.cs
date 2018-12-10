ef853e8252728f6da723b2cf453d7131ab7181ea
Amy Lin <amy2791530@gmail.com>
2018-12-10 12:44:39 +0800

    add dropdown function

---
+++ SmallMinecraft/Assets/Script/AmyLin/menu/DropDownController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class DropDownController : MonoBehaviour {
+    public Dropdown dropdown;
+
+    public void Dropdown_IndexChange( int index)
+    {
+        switch( index)
+        {
+            // go back to game menu
+            case 1:
+                SceneManager.LoadScene(0);
+                break;
+
+            // reset
+            case 2:
+
+            // exit the game
+            case 3:
+                Application.Quit();
+                break;
+        }
+    }
+
+    void start()
+    {
+
+    }
+}
