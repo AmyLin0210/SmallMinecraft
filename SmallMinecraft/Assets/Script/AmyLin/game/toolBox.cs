cc5028250243800f43a7694c89494d24da8a27ae
Amy Lin <amy2791530@gmail.com>
2018-12-10 23:00:02 +0800

    develop the toolbox

--- SmallMinecraft/Assets/Script/AmyLin/game/toolBox.cs
+++ SmallMinecraft/Assets/Script/AmyLin/game/toolBox.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class toolBox : MonoBehaviour {
 
+    public Sprite sprite_soil, sprite_leaves, sprite_wood, sprite_stone, sprite_grass;
+
+    const int soil = 0;
+    const int stone = 1;
+    const int wood = 2;
+    const int grass = 3;
+    const int leaves = 4;
+
+    int CubeNumber = -1;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,10 +25,49 @@ public class toolBox : MonoBehaviour {
 		
 	}
 
+    // when we put the first cube in the box
+    public void AddComponment( int cubeNum)
+    {
+        Button image = gameObject.GetComponentsInChildren<Button>()[0];
+        Image boxImage = image.GetComponentInChildren<Image>();
+
+        CubeNumber = cubeNum;
+
+        // set picture
+        switch (cubeNum)
+        {
+            case soil:
+                boxImage.sprite = sprite_soil;
+                break;
+            case stone:
+                boxImage.sprite = sprite_stone;
+                break;
+            case leaves:
+                boxImage.sprite = sprite_leaves;
+                break;
+            case wood:
+                boxImage.sprite = sprite_wood;
+                break;
+            case grass:
+                boxImage.sprite = sprite_grass;
+                break;
+        }
+
+        // initialize cube number
+        Button buttonCubeNumber = gameObject.GetComponentsInChildren<Button>()[1];
+        buttonCubeNumber.GetComponentInChildren<Text>().text = "1";
+
+    }
+
+    public int GetCubeNumber()
+    {
+        return CubeNumber;
+    }
+
     // the cube number enhance
     public void pickUp()
     {
-        Button number = gameObject.GetComponentInChildren<Button>();
+        Button number = gameObject.GetComponentsInChildren<Button>()[1];
         string num = number.GetComponentInChildren<Text>().text;
 
         number.GetComponentInChildren<Text>().text = (int.Parse(num) + 1).ToString();
@@ -27,18 +76,32 @@ public class toolBox : MonoBehaviour {
     // the cube number reduce
     public void takeOut()
     {
-        Button number = gameObject.GetComponentInChildren<Button>();
+        Button number = gameObject.GetComponentsInChildren<Button>()[1];
         string num = number.GetComponentInChildren<Text>().text;
 
         number.GetComponentInChildren<Text>().text = (int.Parse(num) - 1).ToString();
+
+        if ((int.Parse(num) - 1) == 0)
+            clearBox();
+    }
+
+    // make the toolBox empty
+    public void clearBox()
+    {
+        Button number = gameObject.GetComponentsInChildren<Button>()[1];
+        number.GetComponentInChildren<Text>().text = "";
+
+        Button image = gameObject.GetComponentsInChildren<Button>()[0];
+        Image boxImage = image.GetComponentInChildren<Image>();
+        boxImage = null;
+
+        CubeNumber = -1;
     }
 
     // judge is the cube Empty
     public bool isEmpty()
     {
-        Button number = gameObject.GetComponentInChildren<Button>();
-        string num = number.GetComponentInChildren<Text>().text;
-        if (num == "0")
+        if (CubeNumber == -1)
             return true;
         else
             return false;
--- SmallMinecraft/Assets/Script/Game/CubePickUp.cs
+++ SmallMinecraft/Assets/Script/Game/CubePickUp.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class CubePickUp : MonoBehaviour {
 
+    const int soil   = 0;
+    const int stone  = 1;
+    const int wood   = 2;
+    const int grass  = 3;
+    const int leaves = 4;
+
     public GameObject[] toolBox = new GameObject[5];
 
 	// Use this for initialization
@@ -27,27 +33,27 @@ public class CubePickUp : MonoBehaviour {
                 // which cube has been clicked
                 if( cube.name.IndexOf( "cube_soil" ) != -1)
                 {
-                    PutInToBox( 0 );
+                    PutInToBox( soil );
                     Destroy(cube);
                 }
                 if (cube.name.IndexOf("cube_stone") != -1)
                 {
-                    PutInToBox( 1 );
+                    PutInToBox( stone );
                     Destroy(cube);
                 }
                 if (cube.name.IndexOf("cube_wood") != -1)
                 {
-                    PutInToBox( 2 );
+                    PutInToBox( wood );
                     Destroy(cube);
                 }
                 if (cube.name.IndexOf("cube_grass") != -1)
                 {
-                    PutInToBox( 3 );
+                    PutInToBox( grass );
                     Destroy(cube);
                 }
                 if (cube.name.IndexOf("cube_leaves") != -1)
                 {
-                    PutInToBox( 4 );
+                    PutInToBox( leaves );
                     Destroy(cube);
                 }
             }
@@ -58,6 +64,27 @@ public class CubePickUp : MonoBehaviour {
     // change the image in tool box
     void PutInToBox( int cube )
     {
-        toolBox[cube].GetComponent<toolBox>().pickUp();
+        bool isEmpty = true;
+
+        // there is a same cube in the box
+        for( int i = 0; i < 5 && isEmpty; ++i)
+        {
+            if( toolBox[i].GetComponent<toolBox>().GetCubeNumber() == cube)
+            {
+                isEmpty = false;
+                toolBox[i].GetComponent<toolBox>().pickUp();
+            }
+        }
+
+        // there is no same cube in the cube
+        // but there is a empty toolbox
+        for( int i = 0; i < 5 && isEmpty; ++i)
+        {
+            if( toolBox[i].GetComponent<toolBox>().GetCubeNumber() == -1)
+            {
+                isEmpty = false;
+                toolBox[i].GetComponent<toolBox>().AddComponment(cube);
+            }
+        }
     }
 }
