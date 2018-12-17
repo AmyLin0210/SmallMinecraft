ce28f9fbd439e1af0e90925951e8fccf2d486a8c
Amy Lin <amy2791530@gmail.com>
2018-12-17 21:32:19 +0800

    debug

--- SmallMinecraft/Assets/Script/Game/DayAndNight.cs
+++ SmallMinecraft/Assets/Script/Game/DayAndNight.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayAndNight : MonoBehaviour {
+
+    bool isMonsterAppear;
     float timer;
+    public GameObject MonsterBlood;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,18 +16,26 @@ public class DayAndNight : MonoBehaviour {
 	void Update () {
         transform.Rotate(Vector3.right * Time.deltaTime);
 
-        if(transform.eulerAngles.x < 180)
+        if(transform.eulerAngles.x < 180 && transform.eulerAngles.x > 0)
         {
+            isMonsterAppear = false;
             monster.position = new Vector3(100, 300, 0);
+            MonsterBlood.SetActive(false);
 
         }
 
-        if (transform.eulerAngles.x > 180 && transform.eulerAngles.x < 182)
-            monster.position = new Vector3(0,0,0);
-
-            if (transform.eulerAngles.x > 350)
+        if (transform.eulerAngles.x > 180 && transform.eulerAngles.x < 181 && !isMonsterAppear)
         {
+            isMonsterAppear = true;
+            monster.position = new Vector3(0, 4f, 0);
+            MonsterBlood.SetActive(true);
+        }
+
+        if (transform.eulerAngles.x > 350)
+        {
+            isMonsterAppear = false;
             monster.position = new Vector3(0, 0, 30);
+            MonsterBlood.SetActive(false);
         }
 
     }
--- SmallMinecraft/Assets/Script/Game/Monster.cs
+++ SmallMinecraft/Assets/Script/Game/Monster.cs
@@ -15,8 +15,10 @@ public class Monster : MonoBehaviour {
 
         transform.position = transform.position + (player.position - transform.position) * Time.deltaTime * followSpeed;
         transform.LookAt(player);
-		
-	}
+        if (transform.position.y < 1)
+            transform.position = new Vector3(transform.position.x, 3, transform.position.z);
+
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (Regex.IsMatch(collision.gameObject.name, player.name))
